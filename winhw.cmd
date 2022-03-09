9c84c9e4b29137c9ccce1cf17be6f60a401d53b9
Peter Forret <peter@forret.com>
2022-03-09 19:22:48 +0100

    MOD: winhw.cmd

--- winhw.cmd
+++ winhw.cmd
@@ -3,9 +3,10 @@ setlocal
 set output1=.\%computername%.winhw.txt
 set output2=.\%computername%.sysinfo.txt
 
+echo Creating %output1% ...
 (
 echo ## ComputerSystem
-wmic COMPUTERSYSTEM get Caption,ChassisSKUNumber,DNSHostName,Domain,EnableDaylightSavingsTime,HypervisorPresent,Manufacturer,Model,Name,NumberOfLogicalProcessors,NumberOfProcessors,OEMStringArray,PartOfDomain,SystemFamily,SystemSKUNumber,SystemType,TotalPhysicalMemory,UserName /format:list
+wmic ComputerSystem get Caption,ChassisSKUNumber,DNSHostName,Domain,EnableDaylightSavingsTime,HypervisorPresent,Manufacturer,Model,Name,NumberOfLogicalProcessors,NumberOfProcessors,OEMStringArray,PartOfDomain,SystemFamily,SystemSKUNumber,SystemType,TotalPhysicalMemory,UserName /format:list
 
 echo ## Bios
 wmic BIOS get BIOSVersion,CurrentLanguage,Manufacturer,ReleaseDate,SerialNumber,SMBIOSBIOSVersion,Version /format:list
@@ -14,7 +15,19 @@ echo ## Os
 WMIC OS get BootDevice,BuildNumber,Caption,CSName,Description,InstallDate,Locale,MUILanguages,Name,OperatingSystemSKU,OSArchitecture,SerialNumber,SystemDevice,SystemDrive,TotalVirtualMemorySize,TotalVisibleMemorySize,Version /format:list
 
 echo ## Disks
-wmic LOGICALDISK where drivetype!=4 get deviceid, description, volumename /value
+wmic LogicalDisk where drivetype!=4 get deviceid,description,volumename,size,FreeSpace,VolumeSerialNumber
+wmic LogicalDisk list brief
+
+echo ## CPU
+wmic Cpu get MaxClockSpeed,AddressWidth,NumberOfLogicalProcessors,NumberOfCores,Description,Name /value
+
+echo ## Memory
+WMIC MemoryChip get Speed,Capacity,DeviceLocator,Manufacturer,Model,Name,Partnumber
+
+
+echo ## Screens
+wmic DESKTOPMONITOR get Caption,Description,Name,ScreenHeight,ScreenWidth,DeviceID,MonitorManufacturer
+wmic path Win32_VideoController get CurrentHorizontalResolution,CurrentVerticalResolution,Name,VideoProcessor,VideoModeDescription,DriverDate
 
 echo ## Printers
 WMIC PRINTER LIST brief
@@ -22,7 +35,8 @@ WMIC PRINTER LIST brief
 echo ## Software
 WMIC PRODUCT GET name,vendor,version,InstallDate,InstallLocation | sort /r
 
-
 ) | findstr /v /r "^$" > %output1%
+
+echo Creating %output2% ...
 
 systeminfo /fo list > %output2%
